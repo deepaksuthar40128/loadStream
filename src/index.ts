ff1b2a1f8505ef0788b583e7df79adfe9829aeec
deepak suthar <deepaksuthar40128@gmail.com>
2024-01-19 23:04:47 +0530

    load-stream

---
+++ src/index.ts
@@ -0,0 +1,161 @@
+import express, { Request, Response, Express } from 'express';
+import path from 'path';
+import fs from 'node:fs';
+import { v4 as uuidv4 } from 'uuid';
+interface FileData {
+    [key: string]: string | number | fs.WriteStream;
+}
+
+interface FileInfoStore {
+    [key: string]: FileData;
+}
+
+interface FileCallback {
+    (fileInfo: { fileName: string; fileSize: number; destination: string }): { fileName?: string; destination?: string };
+}
+
+class StreamLoader {
+    #app: Express;
+    #fileInfoStore: FileInfoStore;
+    #cb: FileCallback = (() => ({}));
+    onerror: Function = (e: { [key: string]: string }) => { console.log(e.error) };
+
+    /**
+     *
+     * @param cb callback for file name/destination
+     * @returns Express app
+     * @example
+     * const cb = (file)=>{
+     *      //file = {
+     *      //        fileName:'hello.jpg',
+     *      //        fileSize: 34,(Read Only)
+     *      //        destination:'/'
+     *      //   }
+     *      return {
+     *            fileName: random_string'+file.fileName,//return New File Name
+     *            destination: '/static/upload' //path of your upload dir
+     *      }
+     * }
+     *
+     */
+    constructor(cb?: FileCallback) {
+        this.#app = express();
+        this.#app.use(express.json());
+        this.#fileInfoStore = {};
+
+        if (cb)
+            this.#cb = cb;
+
+        this.#app.get('/loadStream/loadStream.js', (req: Request, res: Response) => {
+            res.sendFile(path.resolve(__dirname + '/static/js/loadStream.js'));
+        });
+        this.#app.post('/loadStream/upload/:userId', this.#handleUpload.bind(this));
+        this.#app.post('/loadStream/handshake', this.#handleHandshake.bind(this));
+        this.#app.get('/loadStream/transfered/:userId', this.#handleTransfered.bind(this));
+    }
+
+    /**
+     * Main function to upload Chunks
+     * @param req Request
+     * @param res Response
+     */
+    async #handleUpload(req: Request, res: Response) {
+        const userId: string = req.params.userId;
+        let fileInfo = this.#fileInfoStore[userId];
+        let chunkData: any = [];
+
+        req.on('data', (chunk: Buffer) => {
+            chunkData.push(chunk);
+        });
+
+        req.on('error', () => {
+            this.onerror({ error: 'Something Wrong during upload chunks' });
+            res.json({ success: false, error: 'Something Wrong during upload chunks' });
+        });
+
+        req.on('end', () => {
+            chunkData = Buffer.concat(chunkData);
+            if (chunkData.length === fileInfo.chunksize) {
+                fileInfo.transferedSize += chunkData.length;
+                (fileInfo.writeStream as fs.WriteStream).write(chunkData);
+                res.json({ success: true });
+            } else if (chunkData.length === (fileInfo.fileSize as number % (fileInfo.chunksize as number))) {
+                fileInfo.transferedSize += chunkData.length;
+                (fileInfo.writeStream as fs.WriteStream).write(chunkData);
+                res.json({ success: true });
+                (fileInfo.writeStream as fs.WriteStream).end();
+                delete this.#fileInfoStore[userId];
+            } else {
+                this.onerror({ error: 'Invalid Chunk Size neither a chunksize nor a last chunksize!' });
+                res.json({ success: false, error: 'Invalid Chunk Size neither a chunksize nor a last chunksize!' });
+            }
+        });
+    }
+
+
+    /**
+     * Intial Handshake to share file details and creating write Stream send back a userId
+     * @param req Request
+     * @param res Response
+    */
+    async #handleHandshake(req: Request, res: Response) {
+        try {
+            const { fileName, fileSize, chunksize } = req.body || {};
+            let fileInfo: FileData = {};
+            fileInfo['fileName'] = fileName;
+            fileInfo['destination'] = '/';
+            fileInfo['fileSize'] = fileSize;
+
+            if (this.#cb !== undefined) {
+                let cbData = this.#cb({ ...(fileInfo as { fileName: string, destination: string, fileSize: number }) });
+                if (cbData.fileName)
+                    fileInfo['fileName'] = cbData.fileName;
+                if (cbData.destination)
+                    fileInfo['destination'] = cbData.destination;
+            }
+
+            fileInfo['chunksize'] = chunksize;
+            fileInfo['transferedSize'] = 0;
+            let userId: string = uuidv4();
+            fileInfo['writeStream'] = fs.createWriteStream(path.join((fileInfo['destination'] as string) + '/' + (fileInfo['fileName'] as string))).on('ready', () => {
+                this.#fileInfoStore[userId] = fileInfo;
+                res.json({ handshake: true, userId });
+            }).on('error', (err) => {
+                this.onerror({ error: err });
+                res.json({ handshake: false, error: err });
+            });
+        } catch (err) {
+            this.onerror({ error: err });
+            res.json({ handshake: false, error: err });
+        }
+    }
+
+
+    /**
+     * To send the data that how much file is transfered till Now.
+     * @param req request
+     * @param res res
+    */
+    async #handleTransfered(req: Request, res: Response) {
+        try {
+            const userId: string = req.params.userId;
+            let fileInfo = this.#fileInfoStore[userId];
+            res.json({ success: true, transfered: fileInfo.transferedSize });
+        } catch (err) {
+            this.onerror({ error: err });
+            res.json({ success: false, error: err });
+        }
+    }
+    /**
+     * 
+     * @returns Express App
+     */
+    load(): Express {
+        return this.#app;
+    }
+
+}
+
+
+export default StreamLoader;
+module.exports = StreamLoader;
---
+++ src/static/js/loadStream.js
@@ -0,0 +1,179 @@
+class loadStream {
+    //options for transfer file
+    #options = {
+        method: 'http',
+        chunksize: 1024 * 1024
+    }
+
+    //global Class Parameters
+    #fileSize = 0;
+    #currentTransferTime = Infinity;
+    #controller;
+    #isStoped = false;
+    #userId = null;
+    //EventListeners
+    onprogress = null;
+    onload = null;
+    onerror = null;
+
+    /**
+     * 
+     * @param {Object} options {uploadMethod:http|websocket,ChunkSize}
+     */
+    constructor(options = null) {
+        this.file = file;
+        if (options) {
+            this.#options.method = options.method ? options.method : 'http';
+            this.#options.chunksize = options.chunksize ? options.chunksize : 1024 * 1024;
+        }
+    }
+
+    /**
+     * 
+     * @param {File|Blob|String} file File to Upload
+     */
+    async upload(file) {
+        this.file = file;
+        this.#fileSize = this.file.size;
+        if (this.#fileSize > 0) {
+            if (this.#options.method === 'http') {
+                let res = await this.#handShake();
+                if (res.handshake) {
+                    this.#userId = res.userId;
+                    this.#uploadFile();
+                } else {
+                    if (this.onerror)
+                        this.onerror({ error: res.error || "Something Wrong During Handshaking!" });
+                }
+            } else if (this.#options.method === 'websocket') {
+                // let socket = io({ path: '/loadStream.io' });
+                console.log("Currently Unavalible!");
+            }
+        }
+    }
+    /**
+     * 
+     * @returns boolen is Stoped Transmitting or not
+     */
+    stop() {
+        if (this.#fileSize > 0 && !this.#isStoped) {
+            this.#isStoped = true;
+            this.#controller.abort();
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * 
+     * @returns boolen is Resume Transmitting or not
+     */
+    resume() {
+        if (this.#fileSize > 0 && this.#isStoped) {
+            this.#isStoped = false;
+            this.#uploadFile();
+            return true;
+        }
+        return false;
+    }
+
+    async #uploadFile() {
+        try {
+            let tansferResponse = await this.#getTransferedSize();
+            if (!tansferResponse.success) {
+                if (this.onerror)
+                    this.onerror({ error: tansferResponse.error || "Something went wrong during Getting old Transfer." });
+                return;
+            }
+
+
+            let transferedSize = tansferResponse.transfered;
+            while (transferedSize < this.#fileSize) {
+                let chunk = this.file.slice(transferedSize, transferedSize + this.#options.chunksize);
+                let res = await this.#uploadChunk(chunk);
+                if (!res.success) {
+                    break;
+                }
+                transferedSize += this.#options.chunksize;
+                let progress = (transferedSize / this.#fileSize) * 100;
+                let time = this.#expectedTime(transferedSize);
+                let speed = this.#uploadSpeed();
+                if (this.onprogress)
+                    this.onprogress({ progress, time: Math.floor(time), speed });
+            }
+            if (transferedSize >= this.#fileSize) {
+                if (this.onload)
+                    this.onload();
+            }
+
+        } catch (err) {
+            if (this.onerror)
+                this.onerror({ error: err });
+        }
+    }
+
+    async #uploadChunk(chunk) {
+        let sTime = Date.now();
+        return new Promise((resolve, reject) => {
+            let reader = new FileReader();
+            reader.readAsArrayBuffer(chunk);
+            reader.onload = async (e) => {
+                let data = e.target.result;
+                try {
+                    this.#controller = new AbortController();
+                    let res = await fetch(`/loadStream/upload/${this.#userId}`, {
+                        method: 'POST',
+                        headers: {
+                            "Content-Type": "application/octet-stream",
+                        },
+                        body: data,
+                        signal: this.#controller.signal
+                    })
+                    if (this.#isStoped) {
+                        resolve({ success: false })
+                    }
+                    let success = await res.json();
+                    this.#currentTransferTime = Date.now() - sTime;
+                    resolve(success);
+                } catch (err) {
+                    resolve({ success: false });
+                }
+            }
+        })
+    }
+
+    #expectedTime(transferedSize) {
+        return Math.max(0, ((this.#fileSize - transferedSize) / this.#options.chunksize) * this.#currentTransferTime) / 1000;
+    }
+
+    #uploadSpeed() {
+        return Math.ceil(this.#options.chunksize / this.#currentTransferTime) * 1000;
+    }
+
+    async #handShake() {
+        return new Promise(async (resolve, reject) => {
+            let handshakeData = {
+                fileName: this.file.name,
+                fileSize: this.#fileSize,
+                chunksize: this.#options.chunksize
+            }
+            let res = await fetch('/loadStream/handshake', {
+                method: 'POST',
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(handshakeData),
+            })
+            res = await res.json();
+            resolve(res);
+        })
+    }
+
+    async #getTransferedSize() {
+        return new Promise(async (resolve, reject) => {
+            let res = await fetch(`/loadStream/transfered/${this.#userId}`)
+            res = await res.json();
+            resolve(res);
+        })
+    }
+}
